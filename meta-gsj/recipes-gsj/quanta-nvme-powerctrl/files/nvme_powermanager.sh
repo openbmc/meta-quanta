ae818518710422ae978f959b634ffb0c10692f4b
Samuel Jiang <Samuel.Jiang@quantatw.com>
2019-04-25 14:43:23 +0800

    meta-gsj: quanta-nvme-powerctrl: Update package process
    Update quanata-nvme-powerctrl process included:
    
    init_once.sh:
    1. Dump input pin information because of confirming HW signal.
    2. HW POWER_U2 default is output high. If detect SSD is plugged, don't
       need to initialize POWER_U2 again.
    3. Add RST_BMC_U2 steps, check PWRGD_U2 is fine, wait 100ms then st
       RST_BMC_U2 high.
    
    nvme_powermanager.sh:
    1. Add RST_BMC_U2 steps, check PWRGD_U2 is fine, wait 100ms then set
       RST_BMC_U2 high.
    2. Independent package for power control delete nvme_main DBus property
       trigger and tranfer led trigger to nvem_main package.
    
    Change-Id: I43dcb5848063c68845acf3753ef7ce31ce50e46a
    Signed-off-by: Samuel Jiang <Samuel.Jiang@quantatw.com>

--- meta-gsj/recipes-gsj/quanta-nvme-powerctrl/files/nvme_powermanager.sh
+++ meta-gsj/recipes-gsj/quanta-nvme-powerctrl/files/nvme_powermanager.sh
@@ -4,7 +4,7 @@ U2_PRESENT_STATUS=( 1 1 1 1 1 1 1 1 )
 U2_PRESENT=( 148 149 150 151 152 153 154 155 )
 POWER_U2=( 195 196 202 199 198 197 127 126 )
 PWRGD_U2=( 161 162 163 164 165 166 167 168 )
-
+RST_BMC_U2=( 72 73 74 75 76 77 78 79 )
 
 function set_gpio_direction(){
     #$1 gpio pin, $2 'in','high','low'
@@ -15,47 +15,73 @@ function read_present_set_related_power(){
     #$1 read present number, $2 output power gpio
     var="${U2_PRESENT_STATUS[$1]}"
     # present 0 is plugged,present 1 is removal
-    if [ "$var" == "0" ];then
+    echo "U2 $i present is ${var}"
+    if [ "$var" -eq 0 ];then
         set_gpio_direction $2 "high"
-    else 
+        sleep 0.2
+        set_gpio_direction $2 "low"
+        sleep 0.2
+        set_gpio_direction $2 "high"
+    else
         set_gpio_direction $2 "low"
     fi
+    sleep 0.2
+    echo "U2 set power is $(cat /sys/class/gpio/gpio$2/value)"
 }
 
 function update_u2_status(){
-  #$1 read present gpio
+  #$1 read present gpio $2 power gpio
   var=$(cat /sys/class/gpio/gpio$2/value)
   U2_PRESENT_STATUS[$1]="$var"
 }
 
+function update_u2_direct(){
+  U2_PRESENT_STATUS[$1]="$2"
+}
+
+function recovery_pwrgd(){
+    present=$2
+    pwrgd=$(cat /sys/class/gpio/gpio$3/value)
+    if [ "$present" -eq "$pwrgd" ];then
+        set_gpio_direction "${POWER_U2[$1]}" "low"
+        sleep 0.2
+        set_gpio_direction "${POWER_U2[$1]}" "high"
+        sleep 0.5
+        pwrgd=$(cat /sys/class/gpio/gpio$3/value)
+        if [ "$present" -eq "$pwrgd" ];then
+            #set fault led
+            set_gpio_direction $4 "low"
+        else
+            sleep 0.1
+            set_gpio_direction $4 "high"
+        fi
+    fi
+}
+
 function check_present_and_powergood(){
     #$2 present gpio, $3 powergood gpio
-    present=$(cat /sys/class/gpio/gpio$2/value)
+    present=$2
+    sleep 0.5
     pwrgd=$(cat /sys/class/gpio/gpio$3/value)
+    echo "U2 $i present is ${present} and powergood is ${pwrgd}"
     path=`expr $1`
-    if [ "$present" -eq 0 ] && [ "$pwrgd" -eq 1 ];then        
-        busctl set-property xyz.openbmc_project.nvme.manager /xyz/openbmc_project/nvme/$path xyz.openbmc_project.Inventory.Item Present b true
-    else        
-        busctl set-property xyz.openbmc_project.nvme.manager /xyz/openbmc_project/nvme/$path xyz.openbmc_project.Inventory.Item Present b false
-            if [ "$present" -eq "$pwrgd" ];then
-                #set fault led
-                busctl set-property xyz.openbmc_project.LED.GroupManager /xyz/openbmc_project/led/groups/led\_u2\_$1\_fault xyz.openbmc_project.Led.Group Asserted b true
-            else
-                busctl set-property xyz.openbmc_project.LED.GroupManager /xyz/openbmc_project/led/groups/led\_u2\_$1\_fault xyz.openbmc_project.Led.Group Asserted b false
-            fi
-        
+    if [ "$present" -eq 0 ] && [ "$pwrgd" -eq 1 ];then
+        sleep 0.1
+        set_gpio_direction $4 "high"
+    else
+        set_gpio_direction $4 "low"
     fi
-    
-
 }
+
+echo "==========Start NVME powermanager service $(date) ==========="
 
 ##Initial U2 present status
 for i in {0..7};
 do 
-    update_u2_status $i "${U2_PRESENT[$i]}"
+    update_u2_status $i ${U2_PRESENT[$i]}
 done
 
-
+echo "==========End Update present status $(date) ================="
 ## Loop while
 while :
 do
@@ -64,10 +90,14 @@ do
     ## 1 scend scan all loop
     sleep 0.125
     read=$(cat /sys/class/gpio/gpio${U2_PRESENT[$i]}/value)
-    if [ "${U2_PRESENT_STATUS[$1]}" != read ];then
-        update_u2_status $i "${U2_PRESENT[$i]}"
+    if [ $read -eq 0 ];then 
+        recovery_pwrgd $i $read "${PWRGD_U2[$i]}" "${RST_BMC_U2[$i]}"
+    fi
+    if [ "${U2_PRESENT_STATUS[$i]}" != "$read" ];then
+        echo "Update present status"
+        update_u2_direct $i "$read"
         read_present_set_related_power $i "${POWER_U2[$i]}"
-        check_present_and_powergood $i "${U2_PRESENT[$i]}" "${POWER_U2[$i]}"
+        check_present_and_powergood $i $read "${PWRGD_U2[$i]}" "${RST_BMC_U2[$i]}"
     fi 
   done
 done
