b5002c4f6033a2df3c30188fca3595a9c14dafb4
Samuel Jiang <Samuel.Jiang@quantatw.com>
2019-04-25 14:43:23 +0800

    meta-gsj: quanta-nvme-powerctrl: Update package process
    Update quanata-nvme-powerctrl process included:
    
    init_once.sh:
    1. Dump input pin information because of confirming HW signal.
    2. HW POWER_U2 default is output high. If detect SSD is plugged, don't
       need to initialize POWER_U2 again.
    3. Add RST_BMC_U2 steps, check PWRGD_U2 is fine, wait 100ms then st
       RST_BMC_U2 high.
    
    nvme_powermanager.sh:
    1. Add RST_BMC_U2 steps, check PWRGD_U2 is fine, wait 100ms then set
       RST_BMC_U2 high.
    2. Independent package for power control delete nvme_main DBus property
       trigger and tranfer led trigger to nvem_main package.
    
    Change-Id: I43dcb5848063c68845acf3753ef7ce31ce50e46a
    Signed-off-by: Samuel Jiang <Samuel.Jiang@quantatw.com>

--- meta-gsj/recipes-gsj/quanta-nvme-powerctrl/files/init_once.sh
+++ meta-gsj/recipes-gsj/quanta-nvme-powerctrl/files/init_once.sh
@@ -19,42 +19,71 @@ function read_present_set_related_power(){
     #$1 read present gpio, $2 output power gpio,$3 output direction
     var=$(cat /sys/class/gpio/gpio$1/value)
     # present 0 is plugged,present 1 is removal
-    if [ "$var" == "0" ];then
-        set_gpio_direction $2 "high"
-    else 
+    if [ "$var" == "1" ];then
         set_gpio_direction $2 "low"
     fi
 }
 
+function check_powergood_update_reset(){
+    #$1 read powergood gpio, $2 output reset gpio
+    var=$(cat /sys/class/gpio/gpio$1/value)
+    echo "After set power check $3 SSD Power Good again: $var"
+    if [ "$var" == "1" ];then
+        sleep 0.1
+        set_gpio_direction $2 "high"
+    else
+        sleep 0.1
+        set_gpio_direction $2 "low"
+    fi
+}
 
-## Initial U2_PRESNET_N
+echo "===========Start Date: $(date)================"
+
+## Initial U2_PRESENT_N
 U2_PRESENT=( 148 149 150 151 152 153 154 155 )
-for i in "${U2_PRESENT[@]}";
+for i in ${!U2_PRESENT[@]};
 do 
-    set_gpio $i;
-    set_gpio_direction $i 'in';
+    set_gpio ${U2_PRESENT[$i]};
+    set_gpio_direction ${U2_PRESENT[$i]} 'in';
+    echo "Read $i SSD present: $(read_gpio_input ${U2_PRESENT[$i]})"
 done
+
+echo "=============End Present GPIO==================" 
 
 ## Initial POWER_U2_EN
 POWER_U2=( 195 196 202 199 198 197 127 126 )
-for i in "${POWER_U2[@]}";
+for i in ${!POWER_U2[@]};
 do
-    set_gpio $i;
+    set_gpio ${POWER_U2[$i]};
 done
+
 
 ## Initial PWRGD_U2
 PWRGD_U2=( 161 162 163 164 165 166 167 168 )
-for i in "${PWRGD_U2[@]}";
+for i in ${!PWRGD_U2[@]};
 do 
-    set_gpio $i;
-    set_gpio_direction $i 'in';
+    set_gpio ${PWRGD_U2[$i]};
+    set_gpio_direction ${PWRGD_U2[$i]} 'in';
+    echo "Read $i SSD Power Good: $(read_gpio_input ${PWRGD_U2[$i]})"
 done
 
-### Initial SSD Power reference U2_PRESNET_N
+echo "=============End Power Good GPIO================="
+
+## Initial RST_BMC_U2
+RST_BMC_U2=( 72 73 74 75 76 77 78 79 )
+for i in ${!RST_BMC_U2[@]};
+do
+    set_gpio ${RST_BMC_U2[$i]};
+done
+
+
+### Initial related Power by Present
 for i in {0..7};
 do
     read_present_set_related_power "${U2_PRESENT[$i]}" "${POWER_U2[$i]}";
-done 
+    check_powergood_update_reset "${PWRGD_U2[$i]}" "${RST_BMC_U2[$i]}" $i;
+done
 
+echo "==============Enad Date: $(date) ================="
 
 exit 0;
