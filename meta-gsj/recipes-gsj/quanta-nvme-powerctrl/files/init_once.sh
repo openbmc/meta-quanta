349d84efc3f2ae4b19729236c386ba129ad80fe5
Samuel Jiang <Samuel.Jiang@quantatw.com>
2019-04-25 14:43:23 +0800

    meta-gsj: quanta-nvme-powerctrl: Update package process
    Update quanata-nvme-powerctrl process included:
    
    init_once.sh:
    1. Dump input pin information because of confirming HW signal.
    2. HW POWER_U2 default is output high. If detect SSD is plugged, don't
       need to initialize POWER_U2 again.
    3. Add RST_BMC_U2 steps, check PWRGD_U2 is fine, wait 100ms then st
       RST_BMC_U2 high.
    
    nvme_powermanager.sh:
    1. Add RST_BMC_U2 steps, check PWRGD_U2 is fine, wait 100ms then set
       RST_BMC_U2 high.
    2. Independent package for power control delete nvme_main DBus property
       trigger and tranfer led trigger to nvem_main package.
    
    Change-Id: I43dcb5848063c68845acf3753ef7ce31ce50e46a
    Signed-off-by: Samuel Jiang <Samuel.Jiang@quantatw.com>

--- meta-gsj/recipes-gsj/quanta-nvme-powerctrl/files/init_once.sh
+++ meta-gsj/recipes-gsj/quanta-nvme-powerctrl/files/init_once.sh
@@ -15,46 +15,87 @@ function read_gpio_input(){
     cat /sys/class/gpio/gpio$1/value
 }
 
+
 function read_present_set_related_power(){
-    #$1 read present gpio, $2 output power gpio,$3 output direction
+    #$1 read present gpio, $2 output power gpio,$3 reset pin, $4 SSD number
     var=$(cat /sys/class/gpio/gpio$1/value)
     # present 0 is plugged,present 1 is removal
-    if [ "$var" == "0" ];then
-        set_gpio_direction $2 "high"
-    else 
+    if [ "$var" == "1" ];then
+        set_gpio_direction $3 "low"
+        sleep 0.1
+        write_clock_gen_chip_0_register $4
         set_gpio_direction $2 "low"
+    else
+        write_clock_gen_chip_1_register $4
     fi
 }
 
+clock_gen_value=$(i2cget -y 8 0x68 0 i 2|sed 's/[0-9]: 0x[0-9a-zA-Z][0-9a-zA-Z] //g')
+echo "Read Clock_gen_value is: $clock_gen_value"
+write_value=$clock_gen_value
 
-## Initial U2_PRESNET_N
+
+function write_clock_gen_chip_1_register(){
+    update_value=$(printf '%x\n' "$((0x01 <<$1))")
+    write_value=$(printf '0x%x\n' "$(($clock_gen_value | 0x$update_value))")
+    echo "Write SSD $1 register value: $write_value"
+    clock_gen_value=$write_value
+    echo "Update Clock gen value: $clock_gen_value"
+}
+
+
+function write_clock_gen_chip_0_register(){
+    update_value=$(printf '%x\n' "$((0x01 <<$1))")
+    verbose_update_value=$(printf '%x\n' "$((~0x$update_value))"|sed 's/ffffffffffffff//g')
+    write_value=$(printf '0x%x\n' "$(($clock_gen_value & 0x$verbose_update_value))")
+    echo "Write SSD $1 register value: $write_value"
+    clock_gen_value=$write_value
+    echo "Update Clock gen value: $clock_gen_value"
+}
+
+
+## Initial U2_PRESENT_N
 U2_PRESENT=( 148 149 150 151 152 153 154 155 )
-for i in "${U2_PRESENT[@]}";
-do 
-    set_gpio $i;
-    set_gpio_direction $i 'in';
+for i in ${!U2_PRESENT[@]};
+do
+    set_gpio ${U2_PRESENT[$i]};
+    set_gpio_direction ${U2_PRESENT[$i]} 'in';
+    echo "Read $i SSD present: $(read_gpio_input ${U2_PRESENT[$i]})"
 done
+
 
 ## Initial POWER_U2_EN
 POWER_U2=( 195 196 202 199 198 197 127 126 )
-for i in "${POWER_U2[@]}";
+for i in ${!POWER_U2[@]};
 do
-    set_gpio $i;
+    set_gpio ${POWER_U2[$i]};
 done
 
 ## Initial PWRGD_U2
 PWRGD_U2=( 161 162 163 164 165 166 167 168 )
-for i in "${PWRGD_U2[@]}";
-do 
-    set_gpio $i;
-    set_gpio_direction $i 'in';
+for i in ${!PWRGD_U2[@]};
+do
+    set_gpio ${PWRGD_U2[$i]};
+    set_gpio_direction ${PWRGD_U2[$i]} 'in';
+    echo "Read $i SSD Power Good: $(read_gpio_input ${PWRGD_U2[$i]})"
 done
 
-### Initial SSD Power reference U2_PRESNET_N
+
+## Initial RST_BMC_U2
+RST_BMC_U2=( 72 73 74 75 76 77 78 79 )
+for i in ${!RST_BMC_U2[@]};
+do
+    set_gpio ${RST_BMC_U2[$i]};
+done
+
+### Initial related Power by Present
 for i in {0..7};
 do
-    read_present_set_related_power "${U2_PRESENT[$i]}" "${POWER_U2[$i]}";
-done 
+    read_present_set_related_power "${U2_PRESENT[$i]}" "${POWER_U2[$i]}" "${RST_BMC_U2[$i]}" $i;
+done
+
+echo "Write final clock gen value: $clock_gen_value"
+i2cset -y 8 0x68 0 $clock_gen_value s
 
 
 exit 0;
--- meta-gsj/recipes-gsj/quanta-nvme-powerctrl/files/nvme_powermanager.sh
+++ meta-gsj/recipes-gsj/quanta-nvme-powerctrl/files/nvme_powermanager.sh
@@ -4,7 +4,7 @@ U2_PRESENT_STATUS=( 1 1 1 1 1 1 1 1 )
 U2_PRESENT=( 148 149 150 151 152 153 154 155 )
 POWER_U2=( 195 196 202 199 198 197 127 126 )
 PWRGD_U2=( 161 162 163 164 165 166 167 168 )
-
+RST_BMC_U2=( 72 73 74 75 76 77 78 79 )
 
 function set_gpio_direction(){
     #$1 gpio pin, $2 'in','high','low'
@@ -15,44 +15,91 @@ function read_present_set_related_power(){
     #$1 read present number, $2 output power gpio
     var="${U2_PRESENT_STATUS[$1]}"
     # present 0 is plugged,present 1 is removal
-    if [ "$var" == "0" ];then
+    echo "U2 $i present is ${var}"
+    if [ "$var" -eq 0 ];then
         set_gpio_direction $2 "high"
-    else 
+    else
+        set_gpio_direction $3 "low"
+        sleep 0.1
+        write_clock_gen_chip_0_register $1
+        sleep 0.005
         set_gpio_direction $2 "low"
     fi
 }
 
 function update_u2_status(){
-  #$1 read present gpio
+  #$1 read present gpio $2 power gpio
   var=$(cat /sys/class/gpio/gpio$2/value)
   U2_PRESENT_STATUS[$1]="$var"
 }
 
-function check_present_and_powergood(){
-    #$2 present gpio, $3 powergood gpio
-    present=$(cat /sys/class/gpio/gpio$2/value)
+function update_u2_direct(){
+  U2_PRESENT_STATUS[$1]="$2"
+}
+
+function recovery_pwrgd(){
+    present=$2
     pwrgd=$(cat /sys/class/gpio/gpio$3/value)
-    path=`expr $1`
-    if [ "$present" -eq 0 ] && [ "$pwrgd" -eq 1 ];then        
-        busctl set-property xyz.openbmc_project.nvme.manager /xyz/openbmc_project/nvme/$path xyz.openbmc_project.Inventory.Item Present b true
-    else        
-        busctl set-property xyz.openbmc_project.nvme.manager /xyz/openbmc_project/nvme/$path xyz.openbmc_project.Inventory.Item Present b false
-            if [ "$present" -eq "$pwrgd" ];then
-                #set fault led
-                busctl set-property xyz.openbmc_project.LED.GroupManager /xyz/openbmc_project/led/groups/led\_u2\_$1\_fault xyz.openbmc_project.Led.Group Asserted b true
-            else
-                busctl set-property xyz.openbmc_project.LED.GroupManager /xyz/openbmc_project/led/groups/led\_u2\_$1\_fault xyz.openbmc_project.Led.Group Asserted b false
-            fi
-        
+    if [ "$present" -eq "$pwrgd" ];then
+        set_gpio_direction "${POWER_U2[$1]}" "low"
+        sleep 0.2
+        set_gpio_direction "${POWER_U2[$1]}" "high"
+        sleep 0.2
+        pwrgd=$(cat /sys/class/gpio/gpio$3/value)
+        if [ "$present" -eq "$pwrgd" ];then
+            write_clock_gen_chip_0_register $1
+            set_gpio_direction $4 "low"
+        else
+            sleep 0.005
+            write_clock_gen_chip_1_register $1
+            sleep 0.1
+            set_gpio_direction $4 "high"
+        fi
     fi
-    
+}
+
+
+function write_clock_gen_chip_1_register(){
+    clock_gen_value=$(i2cget -y 8 0x68 0 i 2|sed 's/[0-9]: 0x[0-9a-zA-Z][0-9a-zA-Z] //g')
+    update_value=$(printf '%x\n' "$((0x01 <<$1))")
+    write_value=$(printf '0x%x\n' "$(($clock_gen_value | 0x$update_value))")
+    i2cset -y 8 0x68 0 $write_value s
+}
+
+
+function write_clock_gen_chip_0_register(){
+    clock_gen_value=$(i2cget -y 8 0x68 0 i 2|sed 's/[0-9]: 0x[0-9a-zA-Z][0-9a-zA-Z] //g')
+    update_value=$(printf '%x\n' "$((0x01 <<$1))")
+    verbose_update_value=$(printf '%x\n' "$((~0x$update_value))"|sed 's/ffffffffffffff//g')
+    write_value=$(printf '0x%x\n' "$(($clock_gen_value & 0x$verbose_update_value))")
+    i2cset -y 8 0x68 0 $write_value s
 
 }
+
+
+function check_present_and_powergood(){
+    #$2 present gpio, $3 powergood gpio
+    present=$2
+    sleep 0.5
+    pwrgd=$(cat /sys/class/gpio/gpio$3/value)
+    echo "U2 $i present is ${present} and powergood is ${pwrgd}"
+    path=`expr $1`
+    if [ "$present" -eq 0 ] && [ "$pwrgd" -eq 1 ];then
+        sleep 0.01
+        write_clock_gen_chip_1_register $1
+        sleep 0.1
+        set_gpio_direction $4 "high"
+    else
+        write_clock_gen_chip_0_register $1
+        set_gpio_direction $4 "low"
+    fi
+}
+
 
 ##Initial U2 present status
 for i in {0..7};
 do 
-    update_u2_status $i "${U2_PRESENT[$i]}"
+    update_u2_status $i ${U2_PRESENT[$i]}
 done
 
 
@@ -64,10 +111,16 @@ do
     ## 1 scend scan all loop
     sleep 0.125
     read=$(cat /sys/class/gpio/gpio${U2_PRESENT[$i]}/value)
-    if [ "${U2_PRESENT_STATUS[$1]}" != read ];then
-        update_u2_status $i "${U2_PRESENT[$i]}"
-        read_present_set_related_power $i "${POWER_U2[$i]}"
-        check_present_and_powergood $i "${U2_PRESENT[$i]}" "${POWER_U2[$i]}"
-    fi 
+    if [ $read -eq 0 ] && [ ${U2_PRESENT_STATUS[$i]} -eq 0 ];then
+        recovery_pwrgd $i $read "${PWRGD_U2[$i]}" "${RST_BMC_U2[$i]}"
+    fi
+    if [ "${U2_PRESENT_STATUS[$i]}" != "$read" ];then
+        echo "Update present status"
+        update_u2_direct $i "$read"
+        read_present_set_related_power $i "${POWER_U2[$i]}" "${RST_BMC_U2[$i]}"
+        if [ $read -eq 0 ];then
+            check_present_and_powergood $i $read "${PWRGD_U2[$i]}" "${RST_BMC_U2[$i]}"
+        fi
+    fi
   done
 done
